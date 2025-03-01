3d14e52df0230ffb7f9d6be9f0edc0cd1535e52a
muromari5123@gmail.com <ABCd7890;7890>
2025-03-01 15:10:34 +0900

    モデルの修正

--- jupyter/app.py
+++ jupyter/app.py
@@ -59,7 +59,10 @@ def find_score(tx, id1, id2):
                f1.popularity AS food1_popularity,
                f2.popularity AS food2_popularity,
                f1.pagerank AS food1_pagerank, 
-               f2.pagerank AS food2_pagerank
+               f2.pagerank AS food2_pagerank,
+               gds.similarity.cosine(f1.word_vector, f2.word_vector) as word_similarity, 
+               gds.similarity.overlap(f1.flavor_vector, f2.flavor_vector) as flavor_similarity
+
     """
     result = tx.run(query4, id1=id1, id2=id2)
     data = result.data()
@@ -71,7 +74,7 @@ def find_score(tx, id1, id2):
 # API route for prediction
 @app.post("/predict/")
 def predict(food_pair: FoodPairRequest):
-    Xcolumns = ["shared_aromas","food1_popularity","food2_popularity","food1_pagerank","food2_pagerank"]
+    Xcolumns = ["shared_aromas","food1_popularity","food2_popularity","food1_pagerank","food2_pagerank", "word_similarity", "flavor_similarity"]
 
     # Neo4j からデータ取得
     with driver.session() as session:
